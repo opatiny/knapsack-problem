f05bc7a2cc6a2707e1ddb150b5872f6873c45850
Oceane Patiny <oceane@patiny.com>
2023-06-13 08:46:49 +0200

    feat: implement printFinalItemsNames

---
+++ src/utils/printFinalItemsNames.ts
@@ -0,0 +1,15 @@
+import { BagItem } from "..";
+import { ObjectSelection } from "../geneticAlgorithm/selectObjects";
+
+export function printFinalItemsNames(
+  data: BagItem[],
+  selection: ObjectSelection
+): void {
+  let finalItemsNames: string[] = [];
+  for (let i = 0; i < selection.length; i++) {
+    if (selection[i]) {
+      console.log(`- ${data[i].name}`);
+    }
+  }
+  console.log("\n");
+}
